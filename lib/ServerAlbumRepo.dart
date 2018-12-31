604353af22f48769b727caadf1017557e3b50eec
Fabio de Matos <fmatosqg@gmail.com>
2018-12-31 18:35:56 +1100

    Upgrade code to flutter beta 0.11.3

--- lib/ServerAlbumRepo.dart
+++ lib/ServerAlbumRepo.dart
@@ -7,10 +7,10 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
 class ServerAlbumRepo extends AlbumRepo {
-
   final _ServerDomain = "http://amazingdomain.net/";
   final _AlbumDirectory = "albums";
-  final _AlbumKspUrl = "album/customrss?url=https://www.reddit.com/r/KerbalSpaceProgram/.rss";
+  final _AlbumKspUrl =
+      "album/customrss?url=https://www.reddit.com/r/KerbalSpaceProgram/.rss";
 
   @override
   List<AlbumDto> getAlbumList() {
@@ -18,10 +18,9 @@ class ServerAlbumRepo extends AlbumRepo {
     List list = new List<AlbumDto>();
 
     list.add(new AlbumDto(
-        thumbnail: "https://scontent-frt3-2.cdninstagram.com/vp/99d744b652e3acbc1da4977d3a3c770c/5B45E495/t51.2885-15/e35/27892594_1977419992285579_3382350220921667584_n.jpg",
-        niceName: "Test album 1"
-    ));
-
+        thumbnail:
+            "https://scontent-frt3-2.cdninstagram.com/vp/99d744b652e3acbc1da4977d3a3c770c/5B45E495/t51.2885-15/e35/27892594_1977419992285579_3382350220921667584_n.jpg",
+        niceName: "Test album 1"));
 
     return list;
   }
@@ -29,9 +28,8 @@ class ServerAlbumRepo extends AlbumRepo {
   @override
   Future<List<AlbumDto>> getAlbumListAsync() async {
     var response = await http.get('${_ServerDomain}${_AlbumDirectory}');
-    final json = JSON.decode(response.body);
+    final json = jsonDecode(response.body);
 
     return AlbumDto.listFromJson(json);
-
   }
 }
